28d673ebdc80d6d9d2092bafda5fdd0d97aec8e5
zeroleaked <zirlysukarno@gmail.com>
2022-09-19 07:59:00 +0700

    regression state tested

--- lib/sctp_fsm/concrete_sctp_states/conc_states/conc_regress_state.cpp
+++ lib/sctp_fsm/concrete_sctp_states/conc_states/conc_regress_state.cpp
@@ -1,4 +1,100 @@
+#include <esp_log.h>
+
 #include "conc_regress_state.h"
+#include "conc_table_state.h"
+#include "sctp_lcd.h"
+
+#define CURSOR_NULL 0
+#define CURSOR_OK 1
+
+#define SUBSTATE_FAIL 0
+#define SUBSTATE_SUCCESS 1
+
+static const char TAG[] = "conc_regress_state";
+
+void ConcRegress::enter(Sctp* sctp)
+{
+	sctp_lcd_clear();
+	cursor = CURSOR_NULL;
+
+	assert(sctp->curve.absorbance != NULL);
+	assert(sctp->curve.concentration != NULL);
+
+
+	if (sctp->curve.points < 2) {
+		ESP_LOGI(TAG, "detected %d points, not enough points", sctp->curve.points);
+
+		sctp_lcd_conc_regress_error(cursor);
+		substate = SUBSTATE_FAIL;
+	}
+	else if ((sctp->curve.points == 2) && (sctp->curve.concentration[1] == 0)) { // this should never be true, nonstandard points don't count as curve.points
+		ESP_LOGI(TAG, "detected 2 points, but 1 is not a standard. Not enough standards");
+	}
+	else {
+		ESP_LOGI(TAG, "curve check");
+		standards_length = sctp->curve.points;
+		ESP_LOGI(TAG, "detected %d standard sample points", standards_length);
+		assert(standards_length <= 15); // MAX_POINTS is 15
+		assert(sctp->curve.points <= 15); // MAX_POINTS is 15
+		if (standards_length < 15) { // last row is not a standard point
+			// check if we can interpolate		
+			interpolate = (sctp->curve.absorbance[standards_length] != 0);
+		}
+		else {
+			interpolate = false; // last row is also a standard. Not interpolating
+		}
+
+		float sum_absorbance = 0;
+		float sum_absorbance_sq = 0;
+		float sum_concentration = 0;
+		float sum_product = 0;
+		for (int i=0; i < standards_length; i++) {
+			ESP_LOGI(TAG, "i=%d, (%f, %f)", i, sctp->curve.absorbance[i], sctp->curve.concentration[i]);
+			sum_absorbance += sctp->curve.absorbance[i];
+			sum_absorbance_sq += sctp->curve.absorbance[i] * sctp->curve.absorbance[i];
+			sum_concentration += sctp->curve.concentration[i];
+			sum_product += sctp->curve.absorbance[i] * sctp->curve.concentration[i];
+		}
+		float divider = standards_length * sum_absorbance_sq - sum_absorbance * sum_absorbance;
+		assert(divider != 0);
+		float gradient = (standards_length * sum_product - sum_absorbance * sum_concentration) / divider;
+		float offset = (sum_concentration * sum_absorbance_sq - sum_absorbance * sum_product) / divider;
+		ESP_LOGI(TAG, "m=%f, b=%f", gradient, offset);
+
+		if (interpolate) {
+			ESP_LOGI(TAG, "interpolating");
+			sctp->curve.concentration[standards_length] = sctp->curve.absorbance[standards_length] * gradient + offset;
+			sctp->curve.points++;
+		}
+
+		sctp_lcd_conc_regress(cursor, sctp->curve, interpolate);
+		substate = SUBSTATE_SUCCESS;
+	}
+}
+
+void ConcRegress::okay(Sctp* sctp)
+{
+	if (cursor == CURSOR_OK) {
+		sctp->setState(ConcTable::getInstance());
+	}
+}
+
+void ConcRegress::arrowDown(Sctp* sctp)
+{
+	cursor = CURSOR_OK;
+	switch (substate) {
+		case SUBSTATE_FAIL: {
+			sctp_lcd_conc_regress_error(cursor);
+			break;
+		}
+		case SUBSTATE_SUCCESS: {
+			sctp_lcd_conc_regress(cursor, sctp->curve, interpolate);
+			break;
+		}
+	}
+}
+
+
 
 SctpState& ConcRegress::getInstance()
 {
--- lib/sctp_fsm/concrete_sctp_states/conc_states/conc_regress_state.h
+++ lib/sctp_fsm/concrete_sctp_states/conc_states/conc_regress_state.h
@@ -6,25 +6,25 @@
 class ConcRegress : public SctpState
 {
 public:
-	void enter(Sctp* sctp) {};
-	void okay(Sctp* sctp) {};
-	void arrowUp(Sctp* sctp) { arrowLeft(sctp); };
-	void arrowDown(Sctp* sctp) { arrowLeft(sctp); };
-	void arrowLeft(Sctp* sctp) {};
-	void arrowRight(Sctp* sctp) { arrowLeft(sctp); };
+	void enter(Sctp* sctp);
+	void okay(Sctp* sctp);
+	void arrowUp(Sctp* sctp) { arrowDown(sctp); };
+	void arrowDown(Sctp* sctp);
+	void arrowLeft(Sctp* sctp) { arrowDown(sctp); };
+	void arrowRight(Sctp* sctp) { arrowDown(sctp); };
 	void refreshLcd(Sctp* sctp, command_t command) {};
 	void exit(Sctp* sctp) {};
 	int id(Sctp* sctp) { return 14; };
 	static SctpState& getInstance();
 
-	uint8_t substate;
-	uint8_t cursor;
 private:
 	ConcRegress() {}
 	ConcRegress(const ConcRegress& other);
 	ConcRegress& operator=(const ConcRegress& other);
+	
+	uint8_t cursor;
+	uint8_t substate;
 
-	TaskHandle_t taskHandle;
-	void * taskParam;
-	QueueHandle_t report_queue;
+	uint8_t standards_length;
+	bool interpolate;
 };
