4fb5fda0a56c1ded6bc2beb553f7736ae1f9aecf
zeroleaked <zirlysukarno@gmail.com>
2022-09-21 11:32:36 +0700

    log check

--- lib/sctp_fsm/concrete_sctp_states/idle_state.cpp
+++ lib/sctp_fsm/concrete_sctp_states/idle_state.cpp
@@ -1,10 +1,15 @@
+#include <esp_log.h>
+
 #include "sctp_lcd.h"
 #include "idle_state.h"
 #include "menu_state.h"
 
+static const char TAG[] = "idle_state"; 
+
 void Idle::enter(Sctp* sctp)
 {
 	sctp_lcd_start();
+	ESP_LOGI(TAG, "Idle::enter: queue=0x%08x", (unsigned) sctp->lcd_refresh_queue);
     xTaskCreatePinnedToCore(sctp->refreshLcdWrapper, "LCD refresh", 2048, sctp, 3, &sctp->task_refresh_lcd, 0);
 }
 
