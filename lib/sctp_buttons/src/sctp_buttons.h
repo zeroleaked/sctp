6f237a6165d8ad60b1bd813c554ff9ae0847775c
alzanaA <farhanialzana@gmail.com>
2022-09-21 23:17:54 +0700

    Display for quantization mode is done

--- lib/sctp_buttons/src/sctp_buttons.h
+++ lib/sctp_buttons/src/sctp_buttons.h
@@ -1,10 +1,18 @@
 #pragma once
 
-#define BUTTON_PIN_DOWN 39
-#define BUTTON_PIN_OK 40
+#define BUTTON_PIN_DOWN 0  //strobe
+#define BUTTON_PIN_OK 1    //trig
+#define BUTTON_THRESHOLD 20
 
+typedef enum {waiting, detected, unreleased, update} button_state;
+
+void pcf8574_init(i2c_dev_t *pcf, uint8_t *pcf_val);
 
 void sctp_buttons_init(QueueHandle_t * button_events);
 
 void lib_test(void);
 
+void button_debounce(   int input,
+                        button_state *state,
+                        volatile int *output,
+                        int *count);
