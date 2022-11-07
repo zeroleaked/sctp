a18558a39313ff0df10c6b175c2e75707838adbe
alzanaA <farhanialzana@gmail.com>
2022-11-07 10:08:31 +0700

    Add some test cases

--- test/fsm/test_spectrum/test_spectrum.cpp
+++ test/fsm/test_spectrum/test_spectrum.cpp
@@ -53,7 +53,55 @@ void spectrum_mvp() {
     TEST_ASSERT_EQUAL(NULL, sctp0.blank_take);
 }
 
+void power_test()
+{
+    Sctp sctp0;
+    TickType_t xLastWakeTime;
+    const TickType_t xFrequency = 300000 / portTICK_RATE_MS;
+    xLastWakeTime = xTaskGetTickCount();
+    sctp0.okay();
+    for(;;) {
+        vTaskDelayUntil(&xLastWakeTime, xFrequency);
+        // test to SpecSample
+        sctp0.okay();
+        TEST_ASSERT_EQUAL(STATE_SPEC_BLANK, sctp0.getCurrentStateId());
+        sctp0.arrowLeft();
+        sctp0.arrowUp();
+        sctp0.okay();
+        ESP_LOGI(TAG, "substate sampling");
+        // TaskHandle_t task;
+        vTaskDelay(6000 / portTICK_RATE_MS);
+        ESP_LOGI(TAG, "idle fin");
+        TEST_ASSERT_EQUAL(STATE_SPEC_SAMPLE, sctp0.getCurrentStateId());
 
+        // test to SpecResult
+        sctp0.arrowLeft();
+        sctp0.arrowUp();
+        sctp0.okay();
+        vTaskDelay(7000 / portTICK_RATE_MS);
+        TEST_ASSERT_EQUAL(STATE_SPEC_RESULT, sctp0.getCurrentStateId());
+
+        // test to SpecSave
+        sctp0.arrowDown();
+        sctp0.okay();
+        TEST_ASSERT_EQUAL(STATE_SPEC_SAVE, sctp0.getCurrentStateId());
+
+        // back to SpecResult
+        vTaskDelay(3000 / portTICK_RATE_MS);
+        sctp0.arrowDown();
+        sctp0.okay();
+        TEST_ASSERT_EQUAL(STATE_SPEC_RESULT, sctp0.getCurrentStateId());
+        sctp0.arrowDown();
+        sctp0.arrowDown();
+        sctp0.arrowDown();
+        sctp0.arrowDown();
+        sctp0.okay();
+        TEST_ASSERT_EQUAL(STATE_MENU, sctp0.getCurrentStateId());
+        TEST_ASSERT_EQUAL(NULL, sctp0.sample_take);
+        TEST_ASSERT_EQUAL(NULL, sctp0.absorbance);
+        TEST_ASSERT_EQUAL(NULL, sctp0.blank_take);
+    }
+}
 
 extern "C" {
 
